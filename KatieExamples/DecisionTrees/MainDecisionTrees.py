d03d10e3eeab3693a6e73737fdbe2ee97033360d
Tigran Vardanyan <vardanyant.89@gmail.com>
2018-03-15 18:42:55 +0400

    start ds

--- KatieExamples/DecisionTrees/MainDecisionTrees.py
+++ KatieExamples/DecisionTrees/MainDecisionTrees.py
@@ -5,29 +5,23 @@ from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture, Show_Image
 from ClassifyHelper import Accuracy
 
-picture_name = "NBclf.png"
+picture_name = "DTclf.png"
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
-########################## Naive Bayes #################################
+########################## decision Trees #################################
 
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split = 50)
+# min_samples_split The minimum number of samples required to split an internal node, how complicated the d.s is
+
 clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-#### draw the decision boundary with the text points overlaid
 prettyPicture(clf, features_test, labels_test, picture_name)
 
 Show_Image(picture_name)
 
 accuracy = Accuracy(clf, features_test, labels_test)
 
-print("Accuracy score of NB for terrain data is : {}".format(accuracy))
-
-
-
-
-    
-
-
-
+print("Accuracy score of DT for terrain data is : {}".format(accuracy))
